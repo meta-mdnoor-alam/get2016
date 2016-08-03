c51560fd6acd2f0f33116b866cab3538be79c1cf
meta-mdnoor-alam <mdnoor.alam@metacube.com>
2016-08-03 18:53:40 +0530

    Menu changes and comments added

--- Display.java
+++ Display.java
@@ -1,6 +1,7 @@
 package Session4;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -12,74 +13,153 @@ public class Display {
 	String nodeFileName;
 	String connectionFileName;
 	
+	Scanner scanner=new Scanner(System.in);
+	
+	/**
+	 * Parameterized constructor.
+	 * @param nodeFileName
+	 * @param connectionFileName
+	 */
 	public Display(String nodeFileName, String connectionFileName) {
 		this.nodeFileName=nodeFileName;
 		this.connectionFileName=connectionFileName;
 	}
 	
-	public int firstMenu(Scanner scanner) {
+	/**
+	 * Method to display file menu to user.
+	 * @param scanner
+	 * @return
+	 */
+	public int displayFirstMenu(Scanner scanner) {
 		int choice;
 		System.out.print("\n\n1. Add your profile.");
 		System.out.print("\n2. View whole graph network.");
-		System.out.print("\n3. View friendlist of someone.");
+		System.out.print("\n3. Search a friend by name.");
 		System.out.print("\n4. Check if two users are friend.");
-		System.out.print("\n5. Trace friend path between two nodes.");
+		System.out.print("\n5. Edit Profile.");
+		System.out.print("\n6. Mutual Friend.");
 		System.out.print("\n\nEnter your choice  :\t");
 		choice=scanner.nextInt();
 		return choice;
 	}
 
+	/**
+	 * Method to check choice of the user and call required function.
+	 * @param choice
+	 * @param scanner
+	 * @throws IOException
+	 */
 	public void operateUponChoice(int choice, Scanner scanner) throws IOException {
 		switch (choice) {
 		case 1:
-			addProfile(scanner);
+			Graph.addProfile(scanner);
 			break;
 		case 2:
-			viewWholeNetwork();
+			displaySocialNetwork();
+			break;
+		case 3:
+			displaySearchName();
+			break;
+		case 4:
+			displayIsFriendMenu();
+			break;
+		case 5:
+			displayEditProfile();
+			break;
+		case 6:
+			displayMutualFriend();
 			break;
 		default:
 			System.out.println("\n\nEnter valid choice...");
-			firstMenu(scanner);
+			displayFirstMenu(scanner);
 		}
 	}
+
 	
-	public void addProfile(Scanner scanner) throws IOException {
-		Graph graph=new Graph(nodeFileName, connectionFileName);
-		Validations validate=new Validations();
-		System.out.print("\n\nEnter the type of profile [organisation/user]  :\t");
-		String type=scanner.next();
-		if (!new Validations().validateType(type)) {
-			addProfile(scanner);
-		}
-		System.out.println("\n\nEnter profile details  :");
-		if (type.equalsIgnoreCase("user")) {
-			System.out.print("\n\nUser's Name  :\t");
-			String name=scanner.next();
-			System.out.print("Age  :\t");
-			int age=scanner.nextInt();
-			System.out.print("Gender  :\t");
-			String gender=scanner.next();
-			System.out.print("City  :\t");
-			String city=scanner.next();
-			if (!validate.validateNameCity(name) || !validate.validateAge(age) || !validate.validateGender(gender) || !validate.validateNameCity(city)) {
-				addProfile(scanner);
-			}
-			graph.addNode("user", new UserProfile(name, city, age, gender));
-		} else {
-			System.out.print("\n\nOrganisation's Name  :\t");
-			String orgName=scanner.next();
-			System.out.print("Sector [public/private]  :\t");
-			String sector=scanner.next();
-			System.out.print("City  :\t");
-			String city=scanner.next();
-			if (!validate.validateNameCity(orgName) || !validate.validateSector(sector) || !validate.validateNameCity(city)) {
-				addProfile(scanner);
-			}
-			graph.addNode("organisation", new OrganisationProfile(orgName, city, sector));
-		}
-	}
 	
-	public void viewWholeNetwork() {
+	/**
+	 * displays the social network
+	 * Shows the name of each user and the all other users who are connected to that user
+	 */
+	public void displaySocialNetwork() {
+		int[][] adjacencyMatrix=AdjacencyMatrix.adjacencyMatrix;
+		List<Node> nodeList=Graph.allNodeList;
+		int totalNodes=nodeList.size();
+		int columns=adjacencyMatrix[0].length;
+		System.out.println("Name \t\tConnected to\n");
 		
+		for(int index=0;index<totalNodes;index++) {
+			System.out.print(nodeList.get(index).getProfile().getName()+"\t\t");
+			for(int colIndex=0;colIndex<columns;colIndex++) {
+				if(adjacencyMatrix[index][colIndex]==1) {
+					System.out.print(nodeList.get(colIndex).getProfile().getName()+",");
+				}
+			}
+			System.out.println();
+		}
+	}
+	
+	/**
+	 * 
+	 * @param node denotes the node Object whose details are to be displayed on the console
+	 */
+	public static void displaySingleNode(Node node) {
+		System.out.println("NodeId :"+node.getNodeId());
+		System.out.println("Name:"+node.getProfile().getName());
+		System.out.println("Place:"+node.getProfile().getCity());
+		String typeOfNode=node.getType();
+		if(typeOfNode.equalsIgnoreCase("User")) {
+			UserNode userNode=(UserNode)node;
+			System.out.println("Type Of Node: User Node");
+			System.out.println("Age  : "+userNode.getUserProfile().getAge()+"\n");
+		} else {
+			OrganisationNode organisationNode=(OrganisationNode)node;
+			System.out.println("Type Of Node : Organisation Node");
+			System.out.println("Secror : "+organisationNode.getOrganisationProfile().getSector()+"\n");
+		}	
+	}
+	
+	public void displayIsFriendMenu() throws IOException {
+		System.out.println("Enter first name");
+      	String firstName=scanner.next();
+      	System.out.println("Enter the second name");
+      	String secondName=scanner.next();
+      	if(Graph.isFriend(firstName, secondName)) {
+    	  System.out.println(firstName+ " and "+secondName+" are friends");
+      	} else {
+    	  System.out.println(firstName+ " and "+secondName+" are not friends");
+      	}
+	}
+	
+	public void displaySearchName() throws IOException {
+		System.out.println("Enter the name to be searched  :");
+		Node node=Graph.searchByName(scanner.next());
+		if(node==null) {
+			System.out.println("No record found");
+		} else {
+			System.out.println("NodeNumber:"+node.getNodeId());
+			System.out.println("Type:"+(node.getType().equalsIgnoreCase("Organisation") ? "Organisation Node" : "User Node"));
+			System.out.println("Name:"+node.getProfile().getName());
+			System.out.println("Place:"+node.getProfile().getCity());
+		}
+	}
+	
+	public void displayEditProfile() throws IOException {
+		System.out.println("Enter the profile you want to edit  :");
+		String profileToBeEdited=scanner.next();
+		Graph.editProfile(profileToBeEdited);
+	}
+	
+	public void displayMutualFriend() {
+		System.out.println("Enter the name of first user");
+		String friend1=scanner.next();
+		System.out.println("Enter the name of second user");
+		String friend2=scanner.next();
+		String mutuals=Graph.mutualFriends(friend1, friend2);
+		if(mutuals.equalsIgnoreCase("")) {
+			System.out.println("Sorry No mutuals found");
+		} else {
+			System.out.println("Mutuals of "+friend1+" and "+friend2+"\n\n"+mutuals);
+		}
 	}
 }
--- SocialNetwork.java
+++ SocialNetwork.java
@@ -8,19 +8,19 @@ import java.util.Scanner;
  * @author Noor
  */
 public class SocialNetwork {
-
 	
+	private final static String connectionFileName="src\\Session4\\connections.txt";
+	private final static String nodeFileName="src\\Session4\\node.txt";
 	
 	/**
+	 * Program main function
 	 * @param args
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
 		Scanner scanner=new Scanner(System.in);
-		String connectionFileName="src\\Session4\\connections.txt";
-		String nodeFileName="src\\Session4\\node.txt";
 		Display display=new Display(nodeFileName, connectionFileName);
-		display.operateUponChoice(display.firstMenu(scanner), scanner);
+		display.operateUponChoice(display.displayFirstMenu(scanner), scanner);
 		scanner.close();
 	}
 
