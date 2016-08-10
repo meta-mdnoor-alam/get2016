0418a8f2495b19b3e230283117a66632efe2fe88
meta-mdnoor-alam <mdnoor.alam@metacube.com>
2016-08-10 13:47:09 +0530

    Comments added

--- Sort/EnumConstants.java
+++ Sort/EnumConstants.java
@@ -1,14 +1,21 @@
 package Session8.Sort;
 
 /**
+ * Class to define enum constants for Sorting.
  * @author Noor
- *
  */
 public class EnumConstants {
+	/**
+	 * Enum for choosing sorting type.
+	 * @author Noor
+	 */
 	public enum SortingType {
 		DEFAULT, COMPARISON_SORT, LINEAR_SORT, EXIT
 	}
 	
+	/**
+	 * Enum for sorting methodology.
+	 */
 	public enum SortingMethodology {
 		DEFAULT, QUICK_SORT, BUBBLE_SORT, RADIX_SORT, COUNTING_SORT
 	}
